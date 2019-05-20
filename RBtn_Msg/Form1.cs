63b14cb20b4b2bd90c902c6aad694494949be305
Suhyeon Lee <suhyeon917917@gmail.com>
2019-05-20 09:49:28 +0900

    #2 라디오 버튼 선택에 따른 메세지박스 스타일이 변하도록 코드 구현

--- RBtn_Msg/Form1.cs
+++ RBtn_Msg/Form1.cs
@@ -11,6 +11,9 @@ namespace RBtn_Msg
 {
     public partial class Form1 : Form
     {
+        MessageBoxButtons mbb;
+        MessageBoxIcon mbi;
+
         public Form1()
         {
             InitializeComponent();
@@ -23,6 +26,26 @@ namespace RBtn_Msg
 
         private void RadioButton1_CheckedChanged(object sender, EventArgs e)
         {
+
+        }
+
+        private void BtnShow_Click(object sender, EventArgs e)
+        {
+            if (this.rbOk.Checked)
+                mbb = MessageBoxButtons.OK;
+            else if (this.rbOkCancel.Checked)
+                mbb = MessageBoxButtons.OKCancel;
+            else if (this.rbYesNo.Checked)
+                mbb = MessageBoxButtons.YesNo;
+
+            if (this.rbError.Checked)
+                mbi = MessageBoxIcon.Error;
+            else if (this.rbInformation.Checked)
+                mbi = MessageBoxIcon.Information;
+            else if (this.rbQuestion.Checked)
+                mbi = MessageBoxIcon.Question;
+
+            MessageBox.Show("메세지 박스를 확인하세요", "알림", mbb, mbi);
 
         }
     }
